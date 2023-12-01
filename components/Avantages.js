dde35d5ab42b4383a6dbe5aac7c1d86aa97f56a2
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-12-01 16:56:31 +0100

    Update Avantages.js

--- components/Avantages.js
+++ components/Avantages.js
@@ -13,8 +13,8 @@ const Avantages = () => {
   );
   const avantages = [
     {
-      title: "Éligibilité TVA réduite",
-      text: "Découvrez rapidement si vous êtes éligible à la TVA à taux réduit pour l'achat de votre résidence principale.",
+      title: "Aides de l'état",
+      text: "Découvrez rapidement les subventions et aides auxquelles vous êtes éligible pour financer vos travaux d'isolation.",
       icon: (
         <ArrowTrendingUpIcon
           className="h-4 w-4 text-yellow-500 mr-3"
@@ -23,8 +23,8 @@ const Avantages = () => {
       ),
     },
     {
-      title: "Prêt à taux zéro simplifié",
-      text: "Bénéficiez d'un accompagnement pour profiter du prêt à taux zéro, une aide précieuse pour financer votre projet immobilier.",
+      title: "Accompagnement personnalisé",
+      text: "Bénéficiez d'un accompagnement pour constituer votre dossier.",
       icon: (
         <HandThumbUpIcon
           className="h-4 w-4 text-yellow-500 mr-3"
