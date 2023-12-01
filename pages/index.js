c6f4c47f27c132c9671aaba58166b3ef2807f511
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-12-01 15:35:24 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
 import BreadCrumb from "../components/BreadCrumb";
 import CTABanner from "../components/CTABanner";
+import CertificationsBanner from "../components/CertificationsBanner";
 import EvaluationBanner from "../components/EvaluationBanner";
 import Faq from "../components/Faq";
 import Hero from "../components/Hero";
+import LogoContainer from "../components/LogoContainer";
 import Basic from "../layout/Basic";
+
+const googleCards = [
+  {
+    title: "Isolation des combles",
+    img: "/combles.png",
+  },
+  {
+    title: "Isolation des rampants",
+    img: "/toit.png",
+  },
+  {
+    title: "Isolation des murs interieurs (ITI)",
+    img: "/mur.png",
+  },
+  {
+    title: "Isolation complete de votre bien",
+    img: "/all.png",
+  },
+];
 
 export default function Home() {
   return (
@@ -12,8 +33,12 @@ export default function Home() {
       metaDescription="Découvrez rapidement votre éligibilité au prêt à taux zéro avec Info PTZ. Notre simulateur facile à utiliser vous aide à estimer votre prêt pour un financement immobilier avantageux. Commencez dès maintenant !"
     >
       <Hero />
+      {/* <CertificationsBanner /> */}
       <BreadCrumb list={["ACCUEIL", "ESTIMATION"]} />
       <EvaluationBanner />
+      <div className="bg-gray-50 rounded-lg my-10">
+        <LogoContainer data={googleCards} />
+      </div>
       <Faq />
       <CTABanner />
     </Basic>
