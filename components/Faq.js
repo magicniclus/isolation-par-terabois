1b12ea3f899a4f1bde23bb9e4bfd286ded5d9046
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-12-01 15:34:55 +0100

    Update Faq.js

--- components/Faq.js
+++ components/Faq.js
@@ -4,70 +4,73 @@ import { MinusSmallIcon, PlusSmallIcon } from "@heroicons/react/24/outline";
 const faqs = [
   {
     id: 1,
-    question: "Pourquoi est-il important de simuler son PTZ sur notre site ?",
+    question:
+      "Pourquoi est-il crucial de simuler les aides à l'isolation sur notre site ?",
     answer:
-      "La simulation de votre PTZ sur notre site vous permet de déterminer avec précision l'ampleur de l'aide que vous pouvez obtenir pour votre projet immobilier. Cela optimise votre plan de financement en prenant en compte les conditions actuelles et les critères spécifiques du PTZ.",
+      "Simuler les aides à l'isolation sur notre site vous aide à évaluer précisément les subventions et les prêts à taux zéro auxquels vous pouvez prétendre pour vos travaux d'isolation. Cette démarche optimise votre budget en prenant en compte les aides actuelles et les spécificités des travaux d'isolation.",
   },
   {
     id: 2,
-    question: "Comment notre site assure-t-il une simulation précise du PTZ ?",
+    question:
+      "Comment notre site garantit-il une simulation précise des aides à l'isolation ?",
     answer:
-      "Notre site utilise des données actualisées sur les barèmes du PTZ, les plafonds de ressources, et les zones géographiques concernées. Nos algorithmes sont conçus pour intégrer tous les critères nécessaires afin de vous fournir une estimation précise de votre éligibilité et du montant du PTZ.",
+      "Notre site utilise des données à jour sur les programmes d'aide, incluant MaPrimeRénov', CEE, et éco-PTZ. Nos algorithmes tiennent compte de tous les critères nécessaires pour vous fournir une estimation précise des aides disponibles pour l'isolation de vos combles, murs intérieurs ou sous toiture. Il est cependant possible pour valider l'estimation de consulter un de nos expert en isolation.",
   },
   {
     id: 3,
     question:
-      "Quelle est la valeur ajoutée d'une simulation PTZ en ligne sur notre site ?",
+      "Quels sont les avantages d'une simulation d'aides à l'isolation sur notre site ?",
     answer:
-      "Notre simulation en ligne vous donne un aperçu rapide et personnalisé de votre capacité d'emprunt avec le PTZ. Elle prend en compte les spécificités de votre projet et vous aide à comprendre comment le PTZ peut s'intégrer dans votre financement global.",
+      "Notre simulation en ligne vous offre une vue personnalisée de vos possibilités de financement pour l'isolation de votre maison. Elle prend en compte les détails de vos travaux et vous guide sur les différentes aides disponibles.",
   },
   {
     id: 4,
     question:
-      "Comment les informations sur le PTZ sont-elles tenues à jour sur notre site ?",
+      "Comment les informations sur les aides à l'isolation sont-elles actualisées sur notre site ?",
     answer:
-      "Nous maintenons des liens étroits avec les institutions financières et suivons les modifications législatives pour actualiser en continu nos simulateurs avec les dernières informations disponibles concernant le PTZ.",
+      "Nous entretenons des relations étroites avec les organismes de financement et suivons les évolutions législatives pour maintenir nos simulateurs à jour avec les dernières informations disponibles sur les aides à l'isolation.",
   },
   {
     id: 5,
     question:
-      "Les informations fournies pour la simulation du PTZ sont-elles sécurisées sur notre site ?",
+      "Les informations fournies pour la simulation des aides à l'isolation sont-elles sécurisées sur notre site ?",
     answer:
-      "Totalement. Nous appliquons des mesures de sécurité rigoureuses pour assurer la confidentialité et la protection de vos données personnelles. Votre vie privée est respectée et vos informations ne sont utilisées que pour la simulation de votre PTZ.",
+      "Absolument. Nous utilisons des protocoles de sécurité avancés pour garantir la confidentialité et la protection de vos données personnelles. Vos informations sont exclusivement utilisées pour la simulation des aides à l'isolation.",
   },
   {
     id: 6,
     question:
-      "Combien de temps faut-il pour réaliser une simulation de PTZ sur notre site ?",
+      "Combien de temps faut-il pour réaliser une simulation d'aides à l'isolation sur notre site ?",
     answer:
-      "Notre simulateur de PTZ est conçu pour être rapide et intuitif. En quelques minutes, vous pouvez obtenir une estimation de votre éligibilité et du montant potentiel de votre PTZ.",
+      "Notre simulateur d'aides à l'isolation est rapide et facile à utiliser. En quelques minutes, vous pouvez obtenir une estimation de votre éligibilité et du montant potentiel des aides.",
   },
   {
     id: 7,
     question:
-      "En quoi la simulation de PTZ sur notre site diffère-t-elle des autres outils disponibles ?",
+      "En quoi notre simulateur d'aides à l'isolation se distingue-t-il des autres outils disponibles ?",
     answer:
-      "Notre simulateur de PTZ se distingue par sa précision, l'actualisation constante de ses données et son ergonomie. Nous visons à fournir une expérience utilisateur sans faille, tout en garantissant des résultats fiables et personnalisés.",
+      "Notre simulateur se démarque par sa précision, la mise à jour constante de ses données, et sa facilité d'utilisation. Nous visons à offrir une expérience utilisateur optimale, avec des résultats fiables et adaptés à votre projet d'isolation.",
   },
   {
     id: 8,
-    question: "La simulation du PTZ est-elle gratuite sur notre site ?",
+    question:
+      "La simulation des aides à l'isolation est-elle gratuite sur notre site ?",
     answer:
-      "Oui, la simulation du PTZ sur notre site est entièrement gratuite. Elle est destinée à vous fournir toutes les informations nécessaires pour planifier votre financement immobilier avec le PTZ.",
+      "Oui, la simulation des aides à l'isolation sur notre site est totalement gratuite. Elle vous offre toutes les informations nécessaires pour planifier vos travaux d'isolation.",
   },
   {
     id: 9,
     question:
-      "Puis-je me fier aux résultats de la simulation de PTZ proposée par notre site ?",
+      "Peut-on se fier aux résultats de la simulation d'aides à l'isolation proposée par notre site ?",
     answer:
-      "Absolument. Nos simulations s'appuient sur les données les plus récentes et sont régulièrement vérifiées pour assurer leur fiabilité. Toutefois, nous recommandons de consulter un conseiller financier pour une analyse approfondie de votre situation.",
+      "L'estimation n'es pas contractuelle. Nos simulations s'appuient sur les données les plus récentes et sont régulièrement mises à jour pour assurer leur fiabilité. Cependant, une consultation avec un expert est recommandée pour une analyse détaillée de votre situation.",
   },
   {
     id: 10,
     question:
-      "Quels avantages y a-t-il à utiliser notre site pour simuler un PTZ plutôt qu'un autre outil ?",
+      "Quels avantages y a-t-il à utiliser notre site pour simuler des aides à l'isolation plutôt qu'un autre outil ?",
     answer:
-      "Notre site est spécialement conçu pour simplifier la simulation de votre PTZ. Nous offrons une interface utilisateur claire, des conseils personnalisés et des informations à jour, ce qui vous permet de prendre les meilleures décisions pour votre financement immobilier.",
+      "Notre site est spécialement conçu pour faciliter la simulation de vos aides à l'isolation. Nous offrons une interface claire, des conseils personnalisés et des informations actualisées, vous permettant de prendre les meilleures décisions pour vos travaux d'isolation.",
   },
 ];
 
