e5352f8fba601c0cbb998a840641209062352f87
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-12-01 18:20:15 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import "../styles/globals.css";
 import { Provider } from "react-redux";
 import store from "../redux/store";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const tagManagerArgs = {
+      gtmId: "GTM-5FSG3SMQ",
+    };
+    TagManager.initialize(tagManagerArgs);
+  }, []);
   return (
     <Provider store={store}>
       <Component {...pageProps} />
